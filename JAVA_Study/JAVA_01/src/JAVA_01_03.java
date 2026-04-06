329e05fac56a7ac47b48265d6360e6033962faef
jeonghyeon0819 <819jeonghyeon819@gmail.com>
2026-04-07 02:32:09 +0900

    0407

---
+++ JAVA_Study/JAVA_01/src/JAVA_01_03.java
@@ -0,0 +1,29 @@
+
+public class JAVA_01_03 {
+	
+	public static void main(String[] args) {
+		
+		int a = 100; // 정수
+		double b = 10.5; // 실수
+		boolean c = false; // 불리언(boolean) true, false 입력 가능
+		char d = 'A'; // 문자 하나
+		String e = "Hello Java"; // String은 앞을 대문자로 써야 한다. 문자열, 문자열을 다루기 위한 특별한 타입
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+		
+		// 변수는 데이터를 다루는 종류에 따라 다양한 형식이 존재하는데, 이를 타입(type)라고 하고 '형식' 또는 '형'이라고 한다.
+		// 예를 들면 int타입, int형식, int형 등으로 부른다.
+		// 특별히 구분하지 않고 섞어서 부르기에 같은 말로 이해하면 된다.
+		
+		// 개발자가 직접 적은 100, 10.5, true, 'A', "Hello Java"와 같은 값을 프로그래밍 용어로 리터럴(literal)이라고 한다.
+		// 변수의 값은 변할 수 있지만 리터럴은 개발자가 직접 입력ㅎ나 고정된 값이기에 리터럴 자체는 변하지 않는다.
+		// 리터럴이라는 단어의 어원이 문자 또는 글자를 의미한다.
+		
+		
+	}
+}
