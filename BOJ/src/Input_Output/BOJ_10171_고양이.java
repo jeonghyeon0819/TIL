020f70d77861f8efe2e2a344030208ba473df4ff
jeonghyeon0819 <819jeonghyeon819@gmail.com>
2026-04-16 23:46:31 +0900

    0416

---
+++ BOJ/src/Input_Output/BOJ_10171_고양이.java
@@ -0,0 +1,25 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class BOJ_10171_고양이 {
+	
+	public static void main(String[] args) {
+		
+		// 브론즈 5
+		// 아래 예제와 같이 고양이를 출력하시오.
+//		\    /\
+//		 )  ( ')
+//		(  /  )
+//		 \(__)|
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
